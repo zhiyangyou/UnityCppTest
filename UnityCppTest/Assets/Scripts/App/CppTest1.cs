88d8aff3b5e5d345746bcb68c1f8bfa5d2156d40
youzhiyang <1454900087@qq.com>
2023-12-17 13:47:34 +0800

    111

--- UnityCppTest/Assets/Scripts/App/CppTest1.cs
+++ UnityCppTest/Assets/Scripts/App/CppTest1.cs
@@ -6,6 +6,7 @@ using App.Utils;
 using Spine.Unity;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Object = UnityEngine.Object;
 
 namespace App
@@ -44,27 +45,41 @@ namespace App
 
         static void ReflectMesh_SetArrayForChannelImpl_Injected()
         {
-            
             Type meshType = typeof(Mesh);
 
             // 获取SetSizedNativeArrayForChannel方法
             MethodInfo mi_SetArrayForChannelImpl_Injected = meshType.GetMethod("SetArrayForChannelImpl_Injected", BindingFlags.Static | BindingFlags.NonPublic);
             MethodInfo mi_Obj2Ptr = typeof(Object).GetMethod("GetPtrFromInstanceID", BindingFlags.Static | BindingFlags.NonPublic);
-            IntPtr ptr =  mi_SetArrayForChannelImpl_Injected.MethodHandle.Value;
+            IntPtr ptr = mi_SetArrayForChannelImpl_Injected.MethodHandle.Value;
             int a = 0;
             Mesh m = new Mesh();
             var param = new object[3];
-            param[0] = (object)m.GetInstanceID();
+            param[0] = m.GetInstanceID();
             param[1] = typeof(Mesh);
             param[2] = null;
-            IntPtr _unity_self =(IntPtr) mi_Obj2Ptr.Invoke(null, param) ;
+            IntPtr _unity_self = (IntPtr)mi_Obj2Ptr.Invoke(null, param);
             Debug.Log(_unity_self);
             Debug.Log($"is mono {param[2]}");
+
+            Mesh.SetVertices();  //的反射平替方法
+            
+            
+            
             // IntPtr _unity_self = Object.MarshalledUnityObject.MarshalNotNull(m);
-            // Mesh.SetArrayForChannelImpl_Injected(_unity_self, channel, format, dim, values, arraySize, valuesStart, valuesCount, flags);
-            // m.SetVertices();
-            //Marshal.GetFunctionPointerForDelegate();
+            //SetVertices with NativeArray should use struct type that is 8 bytes (2x float) in size 
+            Mesh.SetArrayForChannelImpl_Injected(
+                _unity_self,
+                VertexAttribute.Position,
+                VertexAttributeFormat.Float32,
+                2,
+                values,
+                arraySize,      //内存字节长度，并非元素个数
+                valuesStart,    
+                valuesCount,    // 元素个数
+                MeshUpdateFlags.Default
+            );
         }
+
         private void OnGUI()
         {
             //使用这个来装换Unity的对象Handle
@@ -73,7 +88,7 @@ namespace App
             // UnityEngine. (ObjectStore.Get(meshHandle) as UnityEngine.Mesh);
 
             // UnityEngine.Material mat = new Material();
-    
+
             // UnityEngine.Object.Destroy();
             // UnityEngine.Object.DestroyImmediate();
             // UnityEngine.Application.isPlaying;
